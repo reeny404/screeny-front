cd5e6009de40f63cd81c9d5391ca6e5173dacf22
reeny404 <happy2heyon@gmail.com>
2024-10-14 21:11:21 +0900

    feat: 모바일 header fixed되도록

---
+++ components/landing/mobile/Header.tsx
@@ -0,0 +1,40 @@
+"use client";
+import NoticeButtons from "@/components/NoticeButtons";
+import clsx from "clsx";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={clsx(
+        "flex w-full items-center justify-between px-4 py-5 md:flex-row md:py-12 md:pl-28 md:pr-11",
+        "fixed z-20 bg-inherit md:static",
+        "transition-all duration-300 ease-in-out",
+        isScrolled ? "h-24 flex-row" : "h-40 flex-col space-y-6"
+      )}
+    >
+      <div className="flex text-3xl font-semibold md:text-[28px]">
+        <div className="relative hidden h-10 w-10 md:block">
+          <Image src="/logo.png" fill alt="로고" />
+        </div>
+        Screeny
+        <span className="text-carrot">.</span>
+      </div>
+      <NoticeButtons />
+    </header>
+  );
+}
+
+export default Header;
