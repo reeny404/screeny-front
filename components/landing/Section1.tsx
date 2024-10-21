a40396c0900870160295229aa828ac85c1a7709d
reeny404 <happy2heyon@gmail.com>
2024-10-21 20:53:35 +0900

    feat: 모바일/데스크탑 페이지 통합

--- components/landing/Section1.tsx
+++ components/landing/Section1.tsx
@@ -2,7 +2,7 @@
 
 import clsx from "clsx";
 import Image from "next/image";
-import AbsoulteImg from "../AbsoulteImg";
+import AbsoulteImg from "./AbsoulteImg";
 import NoticeButtons from "@/components/NoticeButtons";
 
 const ComponentTypes = [
@@ -45,21 +45,19 @@ function Section1() {
         <NoticeButtons className="hidden md:block" />
       </div>
       <div className="flex flex-col space-y-5 md:space-y-8">
-        <div className="w-[calc(100vw-1.5em)] overflow-hidden">
-          <div className="min-w-[562px]">
-            <div className="xs:justify-center flex w-full space-x-2 text-base-s sm:justify-center md:justify-normal">
-              {ComponentTypes.map((text, i) => (
-                <div
-                  key={i}
-                  className={clsx(
-                    "text-nowrap rounded-full border border-gray-600 px-3 py-2 font-semibold",
-                    i == 0 && "bg-white text-navy"
-                  )}
-                >
-                  {text}
-                </div>
-              ))}
-            </div>
+        <div className="xs:w-screen overflow-hidden md:max-w-[45.5vw]">
+          <div className="xs:justify-center flex w-full space-x-2 text-base-s sm:justify-center md:justify-normal">
+            {ComponentTypes.map((text, i) => (
+              <div
+                key={i}
+                className={clsx(
+                  "text-nowrap rounded-full border border-gray-600 px-3 py-2 font-semibold",
+                  i == 0 && "bg-white text-navy"
+                )}
+              >
+                {text}
+              </div>
+            ))}
           </div>
         </div>
         <div className="relative h-72 w-full lg:h-[444px]">
